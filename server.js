ea643f0227c5de0c6149d6abc070ede2071220dc
MostafaMohamed2001 <mostafamohamed8828@gmail.com>
2024-06-23 01:36:28 +0300

    weebhook

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
-const path = require('path')
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const cors = require('cors')
-const compression = require('compression')
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const compression = require("compression");
+const bodyParser = require("body-parser");
 
+dotenv.config({ path: "config.env" });
+const ApiError = require("./utils/apiError");
+const globalError = require("./middlewares/errorMiddleware");
 
-dotenv.config({ path: 'config.env' });
-const ApiError = require('./utils/apiError');
-const globalError = require('./middlewares/errorMiddleware');
+const dbConnection = require("./config/database");
 
-const dbConnection = require('./config/database');
-
-// Routes 
-const mountRoutes = require('./routes')
-
- 
-
- 
+// Routes
+const mountRoutes = require("./routes");
+const { weebhookCheckOut } = require("./services/orderService");
 
 //Connect to db
 dbConnection();
@@ -26,49 +22,51 @@ dbConnection();
 //Express App
 const app = express();
 
-
 // Enable other domains to access your application
-app.use(cors())
-app.options('*', cors());
+app.use(cors());
+app.options("*", cors());
 
 // compress all responses
-app.use(compression())
- 
+app.use(compression());
+
+//checkout weebhook
+app.post(
+  "/webhook-checkout",
+  express.raw({ type: "application/json" }),
+  weebhookCheckOut
+);
 //Middlewares
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'uploads')))
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-  console.log(`mode => ${process.env.NODE_ENV}`)
-
+app.use(express.static(path.join(__dirname, "uploads")));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`mode => ${process.env.NODE_ENV}`);
 }
 
 //
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //Mount routes
 mountRoutes(app);
 
-app.use('*', (req, res, next) => {
-   next(new ApiError(`Can't find this EndPoint ${req.originalUrl} ` ,400))
-})
-
+app.use("*", (req, res, next) => {
+  next(new ApiError(`Can't find this EndPoint ${req.originalUrl} `, 400));
+});
 
 //Global Error handle middleware
-app.use(globalError)
+app.use(globalError);
 
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
   console.log(`App running running on port ${PORT}`);
-}); 
-
+});
 
 //Event => list => cb  function
 
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   console.log(`unhandledRejection Errors: ${err}`);
   server.close(() => {
-    console.log("Shutting down ...")
+    console.log("Shutting down ...");
     process.exit(1);
-  })
-}) 
+  });
+});
--- services/orderService.js
+++ services/orderService.js
@@ -119,3 +119,20 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
   });
 
 })
+
+
+exports.weebhookCheckOut = asyncHandler(async (req, res, next) => {
+  const sig = req.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECERT);
+  } catch (err) {
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+    
+  }
+  if (event.type === 'checkout.session.completed') {
+    console.log("Create Order Here ..... ")
+  }
+})
