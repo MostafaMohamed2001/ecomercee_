d58dc1d67af0090c4488f2d958f38d33667261fd
MostafaMohamed2001 <mostafamohamed8828@gmail.com>
2024-06-23 12:27:24 +0300

    weebhook423

--- services/orderService.js
+++ services/orderService.js
@@ -132,7 +132,7 @@ const createCartOrder = async (session) => {
 
   const cart = await Cart.findById(cartId);
   
-  const user = await User.findOne({ email: session.email });
+  const user = await User.findOne({ email: session.customer_email });
   console.log(cart , user , cartId , shippingAddress , orderPrice)
   // create order
   const order = await Order.create({
